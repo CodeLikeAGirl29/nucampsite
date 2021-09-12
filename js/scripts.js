3b74bd76cc5dfe46277d3bb52016b97d586ca31c
Lindsey Howard <miss_leading009@yahoo.com>
2021-09-12 06:51:59 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
-        }
+$( function () {
+    //carousel controls
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+      if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+        $(".carousel").carousel('pause');
+        $("#carouselButton").children("i").removeClass("fa-pause");
+        $("#carouselButton").children("i").addClass("fa-play");
+      } else {
+        $(".carousel").carousel('cycle');
+        $("#carouselButton").children("i").removeClass("fa-play");
+        $("#carouselButton").children("i").addClass("fa-pause");
+      }
     });
-});
-
-$('#myModal').modal(options);
+    //reserve button
+    $("#reserveButton").click(function () {
+      $("#reserveModal").modal("show");
+    });
+    //login button
+    $("#loginButton").click(function () {
+      $("#loginModal").modal("show");
+    })
+  })
